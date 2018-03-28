4057542c0c4bcbb9e00dfdb32e3d9c056dc62e91
amberhendriks <ahproefdedag@gmail.com>
2018-03-28 21:32:56 +0200

    Update data_interpretation.py
    Added function plot_variation_distribution

--- data_interpretation.py
+++ data_interpretation.py
@@ -54,7 +54,6 @@ def plot_mutation_classification(df):
     plt.ylabel('Class')
     plt.title('Mutation distribution mutated')
 
-
 def plot_mutation_position(df):
     df.loc[df['Class'] == 7].set_index('Class')\
         .from_start.dropna().sort_values().plot(kind='bar')
@@ -64,6 +63,24 @@ def plot_mutation_position(df):
     plt.xlabel('Gene')
     plt.title('Mutation position (from start) on class 7')
 
+def plot_variation_distriution(df):
+    variation_types = ('Deletion', 'Amplification', 'Fusions', 'Truncating Mutations')
+    df_total = pd.DataFrame()
+    for i in range(len(variation_types)):
+        df_var = pd.DataFrame()
+        for column in df:
+            if column.startswith(variation_types[i]):
+                series = df.loc[df[column] == 1]['Class'].value_counts()
+                df_var[column] = series
+                if i == 0:
+                    df_total = df_var  
+                else:
+                    df_total = df_total.combine_first(df_var)
+    df_total.plot(kind='bar')
+    plt.ylabel('Frequency of variation')
+    plt.xlabel('Class')
+    plt.title('Distribution of specific mutations over the classes')
+    print(df_total)
 
 def main():
     data_file = 'output.csv'
@@ -71,7 +88,8 @@ def main():
     # plot_class_distribution(df)
     # plot_gene_distribution(df)
     # plot_mutation_classification(df)
-    plot_mutation_position(df)
+    # plot_mutation_position(df)
+    plot_variation_distribution(df)
     plt.show()
 
 
